15681bc4cfb11b30a60c07bdd76278ce25c2919e
Jean Duffy <jean.duffy@hotmail.com>
2024-03-28 22:50:13 +0100

    Add queries for highlighting wines, country leaderboard, and top recommendations

--- queries.sql
+++ queries.sql
@@ -4,9 +4,11 @@ order by users_count desc
 limit 10
 
 
+
 /*markdown
 ### 1/7 - We want to highlight 10 wines to increase our sales. Which ones should we choose and why?
 ___
+
 
 */
 
@@ -69,6 +71,7 @@ join vintages on wines.id = vintages.wine_id
 join vintage_toplists_rankings on vintages.id = vintage_toplists_rankings.vintage_id
 join toplists on vintage_toplists_rankings.top_list_id = toplists.id
 where wines.winery_id = 1252	
+
 
 
 
@@ -173,6 +176,37 @@ ORDER BY
     subquery.ratings_average DESC
 
 
+SELECT
+    subquery.wine_name,
+    subquery.vintage_name,
+    subquery.ratings_average,
+    subquery.grape_name
+FROM
+    (
+        SELECT
+            wines.name AS wine_name,
+            wines.ratings_average,
+            grapes.name AS grape_name,
+            vintages.name AS vintage_name,
+            ROW_NUMBER() OVER (PARTITION BY grapes.name ORDER BY wines.ratings_average DESC) AS row_num
+        FROM
+            countries
+            JOIN regions ON countries.code = regions.country_code
+            JOIN wines ON regions.id = wines.region_id
+            JOIN most_used_grapes_per_country ON countries.code = most_used_grapes_per_country.country_code
+            JOIN grapes ON most_used_grapes_per_country.grape_id = grapes.id
+            JOIN vintages ON wines.id = vintages.wine_id
+        WHERE
+            grapes.name IN ('Cabernet Sauvignon', 'Chardonnay', 'Pinot Noir')
+    ) AS subquery
+WHERE
+    subquery.row_num <= 20
+ORDER BY
+    subquery.grape_name,
+    subquery.ratings_average DESC
+
+
+
 /*markdown
 ### 6/7 - We would like to create a country leaderboard. Come up with a visual that shows the average wine rating for each country. Do the same for the vintages.
 ___
@@ -194,22 +228,120 @@ GROUP BY country_name
 ORDER BY average_vintage_rating DESC
 
 
+
 /*markdown
 ### 7/7 - One of our VIP clients likes Cabernet Sauvignon and would like our top 5 recommendations. Which wines would you recommend to him?
 ___
 */
 
 SELECT
-    wines.name AS wine_name,
-    wines.ratings_average,
-    wines.ratings_count
+    vintages.name AS vintage_name,
+    ROUND(AVG(wines.ratings_average), 2) AS ratings_average,
+    ROUND(AVG(vintage_toplists_rankings.rank),2) AS average_rank,
+    COUNT(wines.ratings_average) AS number_of_ratings
 FROM
     wines
 JOIN
-    wineries ON wines.winery_id = wineries.id
+    vintages ON wines.id = vintages.wine_id,
+    vintage_toplists_rankings ON vintages.id = vintage_toplists_rankings.vintage_id,
+    regions ON wines.region_id = regions.id,
+    countries ON regions.country_code = countries.code,
+    most_used_grapes_per_country ON countries.code = most_used_grapes_per_country.country_code,
+    grapes ON most_used_grapes_per_country.grape_id = grapes.id
 WHERE
-    wines.name LIKE '%Cabernet Sauvignon%'
+    grapes.name LIKE '%Cabernet Sauvignon%'
+GROUP BY
+    vintage_name
 ORDER BY
-    wines.ratings_average DESC
-LIMIT 5
+    ratings_average DESC
+
+
+
+SELECT
+    vintages.name AS vintage_name,
+    toplists.name AS toplist_name
+    --ROUND(AVG(wines.ratings_average), 2) AS ratings_average,
+    --ROUND(AVG(vintage_toplists_rankings.rank),2) AS average_rank
+FROM
+    vintages
+JOIN
+    vintage_toplists_rankings ON vintages.id = vintage_toplists_rankings.vintage_id,
+    toplists ON vintage_toplists_rankings.top_list_id = toplists.id
+WHERE
+    vintages.name LIKE '%Cabernet Sauvignon%'
+GROUP BY
+    vintage_name
+--ORDER BY
+    --ratings_average DESC
+
+
+
+SELECT
+    vintages.name AS vintage_name,
+    vintages.ratings_average,
+    vintages.ratings_count,
+    countries.name AS country_name,
+    regions.name AS region_name
+FROM
+    wines
+JOIN
+    vintages ON wines.id = vintages.wine_id,
+    regions ON wines.region_id = regions.id,
+    countries ON regions.country_code = countries.code
+WHERE
+    countries.name LIKE '%France%'
+ORDER BY
+    vintages.ratings_average DESC
+LIMIT 10 
+
+SELECT
+    vintages.name AS vintage_name,
+    vintages.ratings_average,
+    vintages.ratings_count,
+    keywords_wine.count,
+    keywords.*
+FROM
+    wines
+JOIN
+    vintages ON wines.id = vintages.wine_id,
+    keywords_wine ON wines.id = keywords_wine.wine_id,
+    keywords ON keywords_wine.keyword_id = keywords.id
+WHERE
+    --keywords_wine.count >= 50
+    vintage_name == 'Château Lafite Rothschild Pauillac (Premier Grand Cru Classé) 2003'
+GROUP BY
+    keywords.name
+ORDER BY
+    keywords_wine.count DESC, keywords.name ASC
+
+SELECT *
+FROM (
+    SELECT
+        vintages.name AS vintage_name,
+        vintages.ratings_average,
+        vintages.ratings_count,
+        keywords_wine.count,
+        keywords.name AS keyword_name,
+        ROW_NUMBER() OVER (PARTITION BY vintages.name ORDER BY keywords_wine.count DESC) AS row_num
+    FROM
+        wines
+    JOIN
+        vintages ON wines.id = vintages.wine_id,
+        keywords_wine ON wines.id = keywords_wine.wine_id,
+        keywords ON keywords_wine.keyword_id = keywords.id
+    WHERE
+        keywords_wine.count >= 20
+        --vintage_name == 'Château Lafite Rothschild Pauillac (Premier Grand Cru Classé) 2003'
+    GROUP BY
+        vintage_name,
+        keyword_name,
+        vintages.ratings_average,
+        vintages.ratings_count,
+        keywords_wine.count
+) AS subquery
+WHERE
+    row_num <= 3 AND vintage_name LIKE '%Cabernet Sauvignon%'
+ORDER BY
+    vintage_name ASC,
+    count DESC
 
